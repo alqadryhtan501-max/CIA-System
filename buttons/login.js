48cac7f941c6d35a0e196d60d5141f9033750d34
alqadryhtan501-max <alqadryhtan501@gmail.com>
2026-07-31 15:32:43 +0300

    Update login.js

--- buttons/login.js
+++ buttons/login.js
@@ -1,1 +1,17 @@
+const fs = require("fs");
+const path = require("path");
 
+const accountsPath = path.join(__dirname, "../database/accounts.json");
+
+function getAccounts() {
+    return JSON.parse(fs.readFileSync(accountsPath, "utf8"));
+}
+
+function saveAccounts(accounts) {
+    fs.writeFileSync(accountsPath, JSON.stringify(accounts, null, 4));
+}
+
+module.exports = {
+    getAccounts,
+    saveAccounts
+};
